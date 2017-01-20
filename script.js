7b0ad7a411c0b97c57a9bbcdaf35ca2710c3c317
limerentfox <christi@limerentfox.com>
2017-01-20 15:12:57 -0500

    genre info

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ function handleFormSubmit(event) {
 }
 
 function findAndRenderArtists() {
-    const URL = "https://api.spotify.com/v1/search"
-    let $input = $('input#artist-query')
-    let userInput = $input.val()
-    let query = userInput.split(' ').join('+')
-    $input.val('')
+    const searchURL = "https://api.spotify.com/v1/search"
+    let $searchInput = $('input#artist-query')
+    let userInput = $searchInput.val()
+    let searchQuery = userInput.split(' ').join('+')
+    $searchInput.val('')
 
-    $.ajax({url: `${URL}?q=${query}&type=artist`, success: renderArtists})
+    $.ajax({url: `${searchURL}?q=${searchQuery}&type=artist`, success: renderArtists})
 }
 
 function renderArtists(data) {
-    console.log(data)
+
     let artistList = $('ul#artist-list')
     let errorMessageLocation = $('form')
     artistList.html(' ')
@@ -29,8 +29,9 @@ function renderArtists(data) {
         data.artists.items.forEach(function(artist) {
             let name = artist.name
             let genres = artist.genres
-            let fullName = name
-            artistList.append(`<li>${name} - <a href="#" data-name=${name} onclick='getArtistInfo(this)' data-genres= ${genres}>More Info</a></li>`)
+            artistList.append(`<li class="collection-item">${name} - <a href="#" onclick="getArtistInfo(this)" data-genres="${genres}">More Info</a></li>`)
+
+
         })
       } else {
         errorMessageLocation.prepend('<p id="error-message">Sorry, no artists found.</p>')
@@ -38,24 +39,30 @@ function renderArtists(data) {
 
 }
 
+
 function getArtistInfo(event) {
-    let infoTitle = $('h4#artist-info-title')
+
+    let $genreData = event.getAttribute('data-genres')
+    let $genres = new Array()
+    $genres = $genreData.split(',')
+      if ($genres.length == 1) {
+        $genres = $genreData.split('\n')
+      }
+
+    
+    //let name = artist.name
+    //let infoTitle = $('h4#artist-info-title')
+    //infoTitle.append(`<h4>${name}</h4>`)
+    //infoTitle.html(' ')
+
+
+
     let genreList = $('ul#genre-list')
-    infoTitle.html(' ')
     genreList.html(' ')
 
-    let genres = event.getAttribute('data-genres')
-    // let filteredGenres = genres.filter(function(item, pos){
-    //   return genres.indexOf(item)== pos
-    //   })
-    let name = event.getAttribute('data-name')
+            $genres.forEach(function(genre){
 
-    infoTitle.append(`<h4>${name}</h4>`)
-    genreList.append(`<li>${genres}</li>`)
+              genreList.append(`<li>${genre}</li>`)
+            })
 
-}
-
-// side panel thingy to display
-// artist genre
-
-// add a link to each artist in the ul
+      }
